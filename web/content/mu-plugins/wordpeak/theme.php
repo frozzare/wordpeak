2fe29de2933c8c29087e142e881d5985f0b805c3
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-06-27 11:05:36 +0200

    Wrap style cache in init hook and change hook name on theme name hook

--- web/content/mu-plugins/wordpeak/theme.php
+++ web/content/mu-plugins/wordpeak/theme.php
@@ -26,7 +26,7 @@ add_filter( 'theme_root', function () {
  * @return string
  */
 function theme_name() {
-	return apply_filters( 'wordpeak_theme', 'app' );
+	return apply_filters( 'wordpeak_theme_name', 'app' );
 }
 add_filter( 'template', __NAMESPACE__ . '\\theme_name' );
 add_filter( 'option_template', __NAMESPACE__ . '\\theme_name' );
@@ -36,13 +36,15 @@ add_filter( 'stylesheet', __NAMESPACE__ . '\\theme_name' );
 /**
  * Add theme headers information without a stylesheet file.
  */
-wp_cache_add( 'theme' . '-' . md5( WPMU_PLUGIN_DIR . '/../../' . theme_name() ), [
-	'headers' => apply_filters( 'wordpeak_theme_info', [
-		'Author'      => 'Fredrik Forsmo',
-		'AuthorURI'   => 'https://frozzare.com',
-		'Description' => 'Just a theme',
-		'Name'        => 'Custom theme',
-		'Version'     => '1.0.0'
-	] ),
-	'template' => theme_name()
-], 'themes' );
+add_action( 'init', function () {
+	wp_cache_add( 'theme' . '-' . md5( WPMU_PLUGIN_DIR . '/../../' . theme_name() ), [
+		'headers' => apply_filters( 'wordpeak_theme_info', [
+			'Author'      => 'Fredrik Forsmo',
+			'AuthorURI'   => 'https://frozzare.com',
+			'Description' => 'Just a theme',
+			'Name'        => 'Custom theme',
+			'Version'     => '1.0.0'
+		] ),
+		'template' => theme_name()
+	], 'themes' );
+} );
