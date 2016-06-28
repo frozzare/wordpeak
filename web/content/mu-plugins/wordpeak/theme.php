23e980ccb7fded9709788ee508b1805e9aeb8d2a
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-06-28 13:16:22 +0200

    Fix cache issues when using object cache

--- web/content/mu-plugins/wordpeak/theme.php
+++ web/content/mu-plugins/wordpeak/theme.php
@@ -9,7 +9,12 @@ namespace WordPeak;
  * @param  mixed  $value
  */
 function cache_add( $key, $value ) {
-	wp_cache_add( $key . '-' . md5( ABSPATH . '../' . theme_name() ), $value, 'themes' );
+	global $wp_object_cache;
+
+	// Get global cache prefix if it exists.
+	$prefix = isset( $wp_object_cache ) && isset( $wp_object_cache->global_prefix ) ? $wp_object_cache->global_prefix : '';
+
+	wp_cache_add( $key . '-' . md5( ABSPATH . '../' . theme_name() ), $value, $prefix . 'themes' );
 }
 
 /**
@@ -73,3 +78,16 @@ add_action( 'init', function () {
 		'template' => theme_name()
 	] );
 } );
+
+/**
+ * Store `WP_Theme` cache persistently. Since only
+ * one theme is used this is okey.
+ *
+ * @param  bool   $cache_expiration
+ * @param  string $cache_directory
+ *
+ * @return bool
+ */
+add_filter( 'wp_cache_themes_persistently', function( $cache_expiration, $cache_directory ) {
+	return $cache_directory === 'WP_Theme' ? true : $cache_expiration;
+}, 10, 2 );
