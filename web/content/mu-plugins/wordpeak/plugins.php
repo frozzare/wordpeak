54bfeae4ea9eb7e061c011d01edb1fefe41bf36b
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-06-28 10:56:19 +0200

    Add filter to load custom plugin paths

--- web/content/mu-plugins/wordpeak/plugins.php
+++ web/content/mu-plugins/wordpeak/plugins.php
@@ -6,8 +6,21 @@ $plugins = [
 	'foundation/foundation.php'
 ];
 
+/**
+ * Add or remove plugins that should be loaded.
+ *
+ * @param array $plugins
+ */
+$plugins = apply_filters( 'wordpeak_plugins', $plugins );
+$plugins = is_array( $plugins ) ? $plugins : [];
+
 foreach ( $plugins as $plugin ) {
-	$path = ABSPATH . '../' . $plugin;
+	$path = $plugin;
+
+	if ( strpos( $plugin, '/' ) !== 0 ) {
+		$path = ABSPATH . '../' . $plugin;
+	}
+
 	$path = realpath( $path );
 
 	wp_register_plugin_realpath( $path );
